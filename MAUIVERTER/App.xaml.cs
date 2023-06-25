20cbe28e63aa5385bd7b3176956a7046ca0b4adc
yenilikci <m.melihcelik017@outlook.com>
2023-06-25 18:39:20 +0300

    creating the conversions page

--- MAUIVERTER/App.xaml.cs
+++ MAUIVERTER/App.xaml.cs
@@ -8,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new MenuView();
+		MainPage = new ConverterView();
 	}
 }
---
+++ MAUIVERTER/MVVM/Views/ConverterView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUIVERTER.MVVM.Views;
+
+public partial class ConverterView : ContentPage
+{
+	public ConverterView()
+	{
+		InitializeComponent();
+	}
+}
