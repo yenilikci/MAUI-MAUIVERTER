8fa973f763139b782a7f1f9b3fc19cd629ec6f1a
yenilikci <m.melihcelik017@outlook.com>
2023-06-25 18:55:22 +0300

    filling the collections of measurement units

--- MAUIVERTER/MVVM/ViewModels/ConverterViewModel.cs
+++ MAUIVERTER/MVVM/ViewModels/ConverterViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitsNet;
 
 namespace MAUIVERTER.MVVM.ViewModels
 {
-    internal class ConverterViewModel
+    public class ConverterViewModel
     {
+        public string QuantityName { get; set; }
+        public ObservableCollection<string> FromMeasures { get; set; }
+        public ObservableCollection<string> ToMeasures { get; set; }
+        public string CurrentFromMeasure { get; set; }
+        public string CurrentToMeasure { get; set; }
+
+        public ConverterViewModel()
+        {
+            QuantityName = "Length";
+            FromMeasures = LoadMeasures();
+            ToMeasures = LoadMeasures();
+            CurrentFromMeasure = "Meter";
+            CurrentToMeasure = "Centimeter";
+        }
+
+        public ObservableCollection<string> LoadMeasures()
+        {
+            var types = Quantity.Infos
+                .FirstOrDefault(x => x.Name == QuantityName)
+                .UnitInfos
+                .Select(u => u.Name)
+                .ToList();
+            return new ObservableCollection<string>(types);
+        }
     }
 }
--- MAUIVERTER/MVVM/Views/ConverterView.xaml.cs
+++ MAUIVERTER/MVVM/Views/ConverterView.xaml.cs
@@ -1,3 +1,5 @@
+using MAUIVERTER.MVVM.ViewModels;
+
 namespace MAUIVERTER.MVVM.Views;
 
 public partial class ConverterView : ContentPage
@@ -5,5 +7,6 @@ public partial class ConverterView : ContentPage
 	public ConverterView()
 	{
 		InitializeComponent();
+		BindingContext = new ConverterViewModel();
 	}
 }
